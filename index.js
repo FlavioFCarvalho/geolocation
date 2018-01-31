3c674b86676443114e12898c38582f24f5903860
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-01-31 11:44:01 -0200

    Config route all

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.listen(8080, function()){
+server.get("/all", function(req, res, next){
+
+});
+
+server.listen(8080, function(){
 
     console.log("%s listening at %s", server.name, server.url)
-}
+});
